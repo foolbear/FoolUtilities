9b37dd5fef6528d9a192c9c4d97b115fa42f64f8
foolbear <greatfoolbear@gmail.com>
2019-12-20 15:42:05 +0800

    FoolDevice.

--- FoolUtilities/FoolUtilities.swift
+++ FoolUtilities/FoolUtilities.swift
@@ -9,11 +9,11 @@
 import Foundation
 
 func foolPrint<T>(_ message: T, file: String = #file, method: String = #function, line: Int = #line) {
-#if DEBUG
+    #if DEBUG
     let now = Date()
     let dformatter = DateFormatter(); dformatter.dateFormat = "yy-MM-dd HH:mm:ss"
     let time = dformatter.string(from: now)
     let fileName = ((file as NSString).lastPathComponent as NSString).deletingPathExtension
     print("\(time) \(fileName)[\(line)], \(method): \(message)")
-#endif
+    #endif
 }
