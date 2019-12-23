60076430688e8d984f7068579793d44fe9d38801
foolbear <greatfoolbear@gmail.com>
2019-12-23 15:43:44 +0800

    public extension Color {

--- FoolUtilities/FoolColor.swift
+++ FoolUtilities/FoolColor.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import SwiftUI
 
 public extension UIColor {
     
@@ -17,6 +18,27 @@ public extension UIColor {
             blue: CGFloat(rgbHex & 0x0000FF) / 255.0,
             alpha: CGFloat(1.0)
         )
+    }
+    
+    convenience init(rgbaHex: UInt) {
+        self.init(
+            red: CGFloat((rgbaHex & 0xFF000000) >> 24) / 255.0,
+            green: CGFloat((rgbaHex & 0x00FF0000) >> 16) / 255.0,
+            blue: CGFloat((rgbaHex & 0x0000FF00) >> 8) / 255.0,
+            alpha: CGFloat(rgbaHex & 0x000000FF) / 255.0
+        )
+    }
+    
+}
+
+public extension Color {
+    
+    init(rgbHex: UInt) {
+        self.init(UIColor(rgbHex: rgbHex))
+    }
+    
+    init(rgbaHex: UInt) {
+        self.init(UIColor(rgbaHex: rgbaHex))
     }
     
 }
