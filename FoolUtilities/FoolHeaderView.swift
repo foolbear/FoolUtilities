544a8f929378ec17c4beb772689af863c79b573d
foolbear <greatfoolbear@gmail.com>
2020-01-01 23:31:58 +0800

    public var.

--- FoolUtilities/FoolHeaderView.swift
+++ FoolUtilities/FoolHeaderView.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @available(iOS 13.0, *)
 public struct FoolHeaderView<Leading, Title, Trailing>: View where Leading: View, Title: View, Trailing: View {
-    var leading: Leading
-    var title: Title
-    var trailing: Trailing
+    public var leading: Leading
+    public var title: Title
+    public var trailing: Trailing
     
     public var body: some View {
         VStack() {
