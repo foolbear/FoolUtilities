f1fdea4e352bbffb08649fe3c65ed6d1109c0813
foolbear <greatfoolbear@gmail.com>
2020-01-05 15:40:49 +0800

    as public.

--- FoolUtilities/FoolKeyboard.swift
+++ FoolUtilities/FoolKeyboard.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 @available(iOS 13.0, *)
 public class FoolKeyboard: ObservableObject {
-    static let shared = FoolKeyboard()
+    public static let shared = FoolKeyboard()
     @Published public var height: CGFloat = 0
     private var center: NotificationCenter
 
